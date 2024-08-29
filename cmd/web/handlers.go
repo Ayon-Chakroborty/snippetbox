9ec537dde1e973ace1fd7c7e669d4316461d182f
Ayon-Chakroborty <gv5072@wayne.edu>
2024-08-29 15:06:14 -0400

    Added form for creating snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"snippetbox.ayonchakroborty.net/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Server", "Go")
-
 	snippets, err := app.snippets.GetLatest()
 	if err != nil {
 		app.serverError(w, r, err)
@@ -48,16 +48,52 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+	app.render(w,r, http.StatusOK, "create.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "testing model insert"
-	content := "testing model insert"
-	expires := 7
+	//Getting User input
+	err := r.ParseForm()
+	if err != nil{
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil{
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	//Validating user input
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == ""{
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100{
+		fieldErrors["title"] = "This field cannot be more than 100 characters long."
+	}
+
+	if strings.TrimSpace(content) == ""{
+		fieldErrors["content"] = "This field cannot be blank."
+	}
+
+	if expires != 365 && expires != 7 && expires != 1{
+		fieldErrors["expires"] = "This field must be 1, 7, or 365."
+	}
+
+	if len(fieldErrors) > 0{
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
+
+	// Inserting to database
 	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
+	if err != nil{
 		app.serverError(w, r, err)
 		return
 	}
